0c74bed7f35aca85da8be20c47e59bd7081bc9d2
Pinky Gautam <pkgtm1998@gmail.com>
2022-03-06 21:51:30 +0700

    update hadoop setup script

--- setup_hadoop_colab.sh
+++ setup_hadoop_colab.sh
@@ -4,13 +4,13 @@ wget https://downloads.apache.org/hadoop/common/hadoop-3.3.0/hadoop-3.3.0.tar.gz
 # extract hadoop
 tar -xzf hadoop-3.3.0.tar.gz
 
-# # copy hadoop to /usr/local/
-# cp -r hadoop-3.3.0/ /usr/local/
+# copy hadoop to /usr/local/ so that we have hadoop command available
+cp -r hadoop-3.3.0/ /usr/local/
 
 # set java home as environment variable
 echo "export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")" >> \
-/content/hadoop-3.3.0/etc/hadoop/hadoop-env.sh
+/usr/local/hadoop-3.3.0/etc/hadoop/hadoop-env.sh
 
 # add hadoop to path
 python3 -c "import os; \
-os.environ['PATH'] += ':/content/hadoop-3.3.0/bin'"
+os.environ['PATH'] += ':/usr/local/hadoop-3.3.0/bin'"
